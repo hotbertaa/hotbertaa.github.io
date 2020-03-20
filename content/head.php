a5fbbc9a7e46709b568edf0bd58395147f9e622f
hotbertaa <hotbertaa@gmail.com>
2020-03-20 10:49:53 +0900

    php default build

---
+++ content/head.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>Peter Roper Portfolio</title>
+</head>
+<body>
+
+<?php 
+  //////////////////////////////////////////////////
+  // START - HEADER
+  ?>
+	
+	<header class="container-fluid px-0">
+
+	<div id="header" class="container-fluid">
+	<div class="row align-items-center justify-content-start">
+		
+		<div id="mobimenu" class="col-auto py-3 py-md-4 d-lg-none d-block">
+			<a href="#" class="text-decoration-none menuLink"><span class="icn">menu</span></a>
+		</div>
+		
+		<div class="col col-md-auto h-100">
+			<p class="logo pl-xl-5 pr-xl-3 mb-0"><a href="<?php echo $index_url?>" title="homepage" class="text-decoration-none link" data-class="index">
+				<b class="h3">Peter Roper</b>
+			 	<small style="letter-spacing: 1pt;">Portfolio</small>
+			</a></p>
+		</div>
+		
+	
+		<nav class="col">
+			
+			<div class="row align-items-center py-3 py-md-4 pl-5 pr-xl-5">
+			
+			<div class="col">
+				
+				<div class="row h5 d-lg-block d-none">
+  				<!--a class="col-auto mr-xl-4 text-decoration-none scrollTo" href="#intro">Case Studies</a>
+  				<a class="col-auto mr-xl-4 text-decoration-none scrollTo" href="#skills">Skills</a>
+  				<a class="col-auto text-decoration-none scrollTo" href="#contact">Contact</a-->
+  			</div>
+  		</div>
+  		
+  		<div class="col-auto justify-content-end">
+		  			
+  			<a href="<?php echo $menu_url?>" class="d-inline-block">linkedin</a>
+			</div>
+			
+			</div>
+			
+		</nav>
+	
+	</div>
+	</div>
+	</header>
+	<?php 
+	// END - HEADER
+	//////////////////////////////////////////////////
+	?>
+	
+	
+	
+	
+	
+	<?php 
+  //////////////////////////////////////////////////
+  // START - CONTENT
+  ?>
+	<main id="content">
