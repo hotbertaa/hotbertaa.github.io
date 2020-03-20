f40febfd877ded9c43b1a8b5d656639f380d6c97
hotbertaa <hotbertaa@gmail.com>
2020-03-20 12:23:17 +0900

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,38 +1,42 @@
-//$(function(){
-
-//element.addEventListener("scroll", function(){ 
-var st = $(window).scrollTop(); 
-if (st > lastScrollTop){
-  $('body').removeClass("scrollup").addClass("scrolldown");
-} else if (st == 0){
-  $('body').removeClass("scrollup").removeClass("scrolldown");
-} else {
-  $('body').removeClass("scrolldown").addClass("scrollup");
-}
-lastScrollTop = st;
-});
-    
 var lastScrollTop = 0;
-$(window).on('scroll',function(e){
-$('.autohide').show();
-var _scroll = $(window).scrollTop();   
-//if (_scroll >= 1){
-if($(this).scrollTop() >= $('#wrap').offset().top){
-  $('body').addClass("scrollx");
-  $("#wrap").addClass("scrollr").removeClass('hashLink');
-} 
-if($(this).scrollTop() >= $('#wrap').offset().top +125){
-  $('body').removeClass("scrollz").addClass("scrolly");
-}
-if($(this).scrollTop() >= $('#wrap').offset().top +400){
-  $('body').removeClass("scrolly").addClass("scrollz");
-} 
-if(_scroll >= 1){} else {
-	$('body').removeClass("scrollx").removeClass("scrolly").removeClass("scrollz");
-  $("#wrap").removeClass("scrollr");  
-}
+    $(window).on('scroll',function(e){
+        $('.autohide').show();
 
-//});
+        if($('#adventureWin').length > 0 || $('#actWin').length > 0 || $('#venueWin').length > 0 || $('#venWin').length > 0){ } else {
+
+      var _scroll = $(window).scrollTop();   
+      //if (_scroll >= 1){
+      if($(this).scrollTop() >= $('#wrap').offset().top){
+        $('body').addClass("scrollx");
+        $("#wrap").addClass("scrollr").removeClass('hashLink');
+      } 
+      if($(this).scrollTop() >= $('#wrap').offset().top +125){
+        $('body').removeClass("scrollz").addClass("scrolly");
+      }
+      if($(this).scrollTop() >= $('#wrap').offset().top +400){
+        $('body').removeClass("scrolly").addClass("scrollz");
+      } 
+      //else {
+      if (_scroll >= 1){} else {
+            $('body').removeClass("scrollx").removeClass("scrolly").removeClass("scrollz");
+            $("#wrap").removeClass("scrollr");  
+      }
+      }
+    	
+    	
+		//element.addEventListener("scroll", function(){ 
+    var st = $(window).scrollTop(); 
+    if (st > lastScrollTop){
+      $('body').removeClass("scrollup").addClass("scrolldown");
+    } else if (st == 0){
+    	$('body').removeClass("scrollup").removeClass("scrolldown");
+    } else {
+      $('body').removeClass("scrolldown").addClass("scrollup");
+    }
+    lastScrollTop = st;
+		//}, false);
+    
+    });
 
 $('.scrollTo').on('click', function(){
     //$('html,body').animate({scrollTop: $($(this).attr('href')).offset().top},500);
